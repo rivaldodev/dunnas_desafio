d813859eae08c7ad00f55553ca18586175fe74c3
rivaldodev <rivaldo.freitas.106@gmail.com>
2025-08-25 22:02:15 -0300

    teste rápido da tela de login

---
+++ src/test/java/com/dunnas/LoginPageTest.java
@@ -0,0 +1,25 @@
+package com.dunnas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LoginPageTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void deveCarregarPaginaLogin() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(org.hamcrest.Matchers.containsString("Acesso")));
+    }
+}

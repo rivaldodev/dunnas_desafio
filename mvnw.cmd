7ac2249283f3f695f46ea8db098ddd0033cc581e
rivaldodev <rivaldo.freitas.106@gmail.com>
2025-08-25 22:01:03 -0300

    ajuste wrapper maven pra build rodar liso

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,21 +1,30 @@
-@ECHO OFF
+@REM ----------------------------------------------------------------------------
+@REM Maven Start Up Batch script for Windows
+@REM Simplificado para o desafio (baseado no wrapper oficial 3.2.0)
+@REM ----------------------------------------------------------------------------
+@echo off
 setlocal
+
 set MAVEN_OPTS=%MAVEN_OPTS% -Xms128m -Xmx512m
 
 set SCRIPT_DIR=%~dp0
-set WRAPPER_JAR="%SCRIPT_DIR%.mvn\wrapper\maven-wrapper.jar"
+set WRAPPER_JAR=%SCRIPT_DIR%.mvn\wrapper\maven-wrapper.jar
 set WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
 
-if exist %WRAPPER_JAR% (
-  set DOWNLOAD=F
-) else (
-  set DOWNLOAD=T
+if not exist "%WRAPPER_JAR%" (
+  if not exist "%SCRIPT_DIR%.mvn\wrapper" mkdir "%SCRIPT_DIR%.mvn\wrapper" >NUL 2>&1
+  powershell -Command "Invoke-WebRequest -UseBasicParsing -OutFile '%WRAPPER_JAR%' https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.2.0/maven-wrapper-3.2.0.jar" || (
+    echo Falha ao baixar maven-wrapper.jar & exit /b 1
+  )
 )
 
-if %DOWNLOAD%==T (
-  powershell -Command "Invoke-WebRequest -OutFile %WRAPPER_JAR% https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.2.0/maven-wrapper-3.2.0.jar"
+for %%i in (java.exe) do set JAVA_EXE=%%~$PATH:i
+if not defined JAVA_EXE (
+  echo Java não encontrado no PATH & exit /b 1
 )
 
-set JAVA_EXE=java.exe
+set MAVEN_PROJECTBASEDIR=%SCRIPT_DIR%
+set MAVEN_PROJECTBASEDIR=%MAVEN_PROJECTBASEDIR:~0,-1%
 
-%JAVA_EXE% %MAVEN_OPTS% -classpath %WRAPPER_JAR% %WRAPPER_LAUNCHER% %*
+"%JAVA_EXE%" %MAVEN_OPTS% -classpath "%WRAPPER_JAR%" -Dmaven.multiModuleProjectDirectory="%MAVEN_PROJECTBASEDIR%" %WRAPPER_LAUNCHER% %*
+endlocal
